9e91994a93d9b71777cd3189ce9f69cd64e6842a
nkawadka <nkawadka@LIN25007756.corp.capgemini.com>
2021-11-10 02:57:05 +0530

    sc3rd

---
+++ SpringCore_3/src/main/java/SpringCore3/BankAccountServicelmpl.java
@@ -0,0 +1,25 @@
+package SpringCore3;
+
+public class BankAccountServicelmpl implements BankAccountService {
+
+	public double withdraw(long accountld, double balance) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double deposit(long accountld, double balance) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getBalance(long accountld) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean fundTransfer(long fromAccount, long toAccount, double amont) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ SpringCore_3/src/main/java/SpringCore3/Test.java
@@ -0,0 +1,17 @@
+package SpringCore3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class Test {
+	private static ApplicationContext context;
+	public static void main(String[] args) {
+		 
+		context = new ClassPathXmlApplicationContext("bean.xml");
+		BankAccount account = (BankAccount)context.getBean("Account");
+		System.out.println(account.accountBalance);
+		BankAccountcontroller account2 = (BankAccountcontroller)context.getBean("controller");
+		System.out.println(account2.getBalance(10));
+	}
+
+}
