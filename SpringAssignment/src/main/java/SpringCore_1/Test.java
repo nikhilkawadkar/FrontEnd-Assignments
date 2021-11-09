8baf69a499f4c9d8eb6c35369beee04bd52dc5b4
nkawadka <nkawadka@LIN25007756.corp.capgemini.com>
2021-11-10 00:13:21 +0530

    Spring core

---
+++ SpringAssignment/src/main/java/SpringCore_1/Test.java
@@ -0,0 +1,21 @@
+package SpringCore_1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+ 
+public class Test {
+	private static ApplicationContext context;
+	public static void main(String[] args) {
+		 
+		context = new ClassPathXmlApplicationContext("SpringCore_1_bean.xml");
+		
+		//using setter injection
+		//Customer customer = (Customer)context.getBean("customer");
+		//customer.displayCustomer();
+		
+		Customer customer2 = (Customer)context.getBean("customerConstructor");
+		customer2.displayCustomer();
+	}
+
+}
